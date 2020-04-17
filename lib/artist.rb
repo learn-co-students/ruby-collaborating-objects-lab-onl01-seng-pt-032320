50a6e7bd60700f3ac371fa5b2b861ca2238bd38f
Evan Smith <professionalemail@zoho.com>
2020-04-17 22:12:46 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,35 @@
+class Artist
+  attr_accessor :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def add_song(song)
+    song.artist = self
+  end
+  
+  def self.find_or_create_by_name(name)
+    if all.none? {|artist| artist.name == name}
+      Artist.new(name)
+    else 
+      all.detect {|artist| artist.name == name}
+    end
+  end
+  
+  def print_songs
+    Song.all.each do |song|
+      puts "#{song.name}" if song.artist == self
+    end
+  end
+  
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+  
+  def self.all
+    @@all
+  end
+end
