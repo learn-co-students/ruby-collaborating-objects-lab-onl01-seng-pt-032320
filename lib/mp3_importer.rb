e6af8e869730e8928169181488104536fe483255
Christian Pardo <chrispardopersonal@gmail.com>
2020-04-21 00:47:44 +0000

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,18 @@
+class MP3Importer
+  
+  attr_accessor :poth 
+  
+  def initialize(path)
+    @path = path 
+    
+  end
+  
+  def self.files 
+    
+  end
+  
+  def self.import 
+    
+  end
+  
+end
