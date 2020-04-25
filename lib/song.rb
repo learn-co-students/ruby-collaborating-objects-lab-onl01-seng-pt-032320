41544737c3b70a7f87637b2c7414c4b001b409d8
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-04-25 00:23:50 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,5 @@
 class Song
-attr_accessor :name
-attr_reader :artist
+attr_accessor :name, :artist
 @@all = []
 
 def initialize(name)
@@ -12,8 +11,9 @@ def self.all
   @@all
 end
 
-def artist=(artist)
-  @artist = artist
+def artist_name=(name)
+  self.artist = Artist.find_or_create_by_name(name)
+  self.artist.add_song(self)
 end
 
 def self.new_by_filename(file_name)
@@ -21,7 +21,7 @@ def self.new_by_filename(file_name)
     song_array = file_name.split(" - ")
     artist = Artist.new(song_array[0])
     song = self.new(song_array[1])
-    song.artist = artist
+    song.artist_name = artist
     song
 end
 
