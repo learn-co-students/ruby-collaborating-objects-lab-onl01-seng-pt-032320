b95b0eefef620877a444e3e156bee4d99b0e0a9d
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-04-25 00:13:39 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -20,6 +20,7 @@ def self.new_by_filename(file_name)
     file_name.slice!(/.mp3/)
     song_array = file_name.split(" - ")
     artist = Artist.new(song_array[0])
+    artist.add_song(song_array[1])
     song = self.new(song_array[1])
     song.artist = artist
     song
